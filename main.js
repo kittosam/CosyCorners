c3d7cb8c279dbf1f2aee505c1796fa28aae34956
kittosam <32850736+kittosam@users.noreply.github.com>
2023-08-16 22:15:18 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function displayName() {
+    var name = document.getElementById("name").value;
+    if (name) {
+        alert("Hello " + name + ", Welcome to Cosy Corners!");
+    } else {
+        alert("Hello, please enter your name!");
+    }
+}
+
